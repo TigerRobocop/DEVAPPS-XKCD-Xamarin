ea4141c9649088d720968f5c346ae0b326d462db
Lívia Souza <lmrs@cesar.org.br>
2018-11-03 09:47:13 -0300

    added markup extensions

---
+++ DEVAPPS_Xamarin/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DEVAPPS_Xamarin
+{
+    public class Constants
+    {
+        public static Color backgroundColor = Color.Yellow;
+        public static LayoutOptions centerAlignment = LayoutOptions.CenterAndExpand;
+        public static FontAttributes fontBold = FontAttributes.Bold;
+    }
+}
